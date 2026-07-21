61cf9c86d02599a29b148c580b95434dae0b61e1
Nekoutb <boma.nekout@cm-ea.com>
2026-07-22 00:19:13 +0100

    Add customer support email configuration

--- backend/tests/Feature/ExampleTest.php
+++ backend/tests/Feature/ExampleTest.php
@@ -14,6 +14,9 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertSee('htsmscustomerservice@cm-ea.com')
+            ->assertSee('mailto:htsmscustomerservice@cm-ea.com', false);
     }
 }
